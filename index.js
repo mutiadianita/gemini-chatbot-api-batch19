5ea0b6a203329155f84f87688076184227ef0a4e
mutiadianita <mutia.ayu@sg-dsa.com>
2026-06-11 18:00:17 +0700

    add script start

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => { console.log(`Server ready on http://localhost:${PORT}`); });
 
 app.post('/api/chat', async (req, res) => {
